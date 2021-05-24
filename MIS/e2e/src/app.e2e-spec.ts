a2b2c51e50deca12e4ecef55f17d4a7d930b2d24
Linksonder <linksonder@gmail.com>
2021-05-24 15:22:39 +0200

    changes voor e2e

--- MIS/e2e/src/app.e2e-spec.ts
+++ MIS/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('MIS app is running!');
+    expect(await page.getTitleText()).toEqual('MIS');
   });
 
   afterEach(async () => {
